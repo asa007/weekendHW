74fd183edd0c5950c3c4c31cdf9cbbf7ee4d3b03
渡辺朝子 <asakowatanabe@watanabeasakonoMacBook-Air.local>
2018-03-10 09:30:34 +0800

    htmlをphpに変換。cssと一つにまとめました。データベースも実装し、おおまかな一言掲示板は完成。

--- hw.php
+++ hw.php
@@ -1,3 +1,47 @@
+
+
+<?php
+// ここにDBに登録する処理を記述する
+if(!empty($_POST)){
+
+
+$clubname=htmlspecialchars($_POST['clubname']);
+$lylics=htmlspecialchars($_POST['lylics']);
+$music=$_POST['music'];
+
+
+ // １．データベースに接続する
+  $dsn = 'mysql:dbname=online_bbs;host=localhost';
+  $user = 'root';
+  $password='';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+
+  // ２．SQL文を実行する
+  $sql = "INSERT INTO `chants` (`clubname`, `lylics`, `music`) VALUES (?, ?, ?);";
+
+  //プリペアーステートメント
+  $data=array($clubname,$lylics,$music);
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute($data);
+
+  // ３．データベースを切断する
+  $dbh = null;
+
+
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -44,7 +88,7 @@
       <!-- 画面左側 -->
       <div class="col-md-3 content-margin-top">
         <!-- form部分 -->
-        <form action="bbs.php" method="post">
+        <form action="hw.php" method="post">
           <!-- clubname -->
           <div class="form-group">
             <div class="input-group">
@@ -67,7 +111,7 @@
             </div>
           </div>
           <!-- 投稿ボタン -->
-          <button type="submit" class="btn btn-primary col-xs-12" disabled>投稿</button>
+          <button type="submit" class="btn btn-primary col-xs-12" >投稿</button>
         </form>
       </div>
 
@@ -85,10 +129,85 @@
                       <p>俺たちが　大阪さ　青と黒　俺らだけ</p>
                       <iframe width="560" height="315" src="https://www.youtube.com/embed/HywAFR5IlvA?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                   </div>
+
+<?php 
+
+// １．データベースに接続する
+$dsn = 'mysql:dbname=online_bbs;host=localhost';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->query('SET NAMES utf8');
+
+// ２．SQL文を実行する
+$sql = 'SELECT * FROM `chants`';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+//データ取得
+$survey_line = array();
+while(1){
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+  //取得できるデータが何もなくなったら処理を終了する
+  if($rec==false){
+    break;
+  }
+
+  $_clubname=$rec['clubname'];
+  $_lylics=$rec['lylics'];
+  $_music=$rec['music'];
+
+
+  $survey_line=[];
+  $survey_line = "<div class='timeline-label label-sita'>
+                    <h2>
+                      <a href='#'> $_clubname</h2>
+                    <p>
+                      $_lylics
+                    </p>
+                    <p>
+                      <iframe width='560' height='315' src= $_music frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>
+                    </p>
+                  </div>";
+    echo $survey_line;
+
+}
+
+
+// $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+// $survey_line[]=$rec;
+// // var_dump($rec);
+
+
+// $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+// $survey_line[]=$rec;
+// // var_dump($rec);
+
+// ３．データベースを切断する
+$dbh = null;
+
+?>
+
+
+
+
+
+
+
               </div>
           </article>
 
-          <article class="timeline-entry begin">
+         
+
+
+
+
+
+
+
+
+
+ <article class="timeline-entry begin">
               <div class="timeline-entry-inner">
                   <div class="timeline-icon" style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg);">
                       <i class="entypo-flight"></i> +
@@ -105,9 +224,8 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="assets/js/bootstrap.js"></script>
-  <script src="assets/js/form.js"></script>
+
 </body>
 </html>
-
 
 
--- oneline_bbs-master/bbs_no_css.php
+++ oneline_bbs-master/bbs_no_css.php
@@ -1,20 +1,55 @@
-<?php
-  // ここにDBに登録する処理を記述する
 
+ 
+<?php 
+// ここにDBに登録する処理を記述する
+$clubname=htmlspecialchars($_POST['clubname']);
+$lylics=htmlspecialchars($_POST['lylics']);
+// $music=htmlspecialchars($_POST['music']);
+
+ // １．データベースに接続する
+  $dsn = 'mysql:dbname=online_bbs;host=localhost';
+  $user = 'root';
+  $password='';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+
+  // ２．SQL文を実行する
+  $sql = "INSERT INTO `survey` (`clubname`, `lylics`, `music`) VALUES (?, ?, ?);";
+
+  //プリペアーステートメント
+  $data=array($clubname,$lylics,$music);
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute($data);
+
+  // ３．データベースを切断する
+  $dbh = null;
 ?>
+
+<?php 
+$nickname=$_POST['clubname'];
+$email=$_POST['lylics'];
+$content=$_POST['music'];
+ ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>セブ掲示版</title>
+  <title>Jリーグチャント集</title>
 </head>
 <body>
     <form method="post" action="">
-      <p><input type="text" name="nickname" placeholder="nickname"></p>
-      <p><textarea type="text" name="comment" placeholder="comment"></textarea></p>
-      <p><button type="submit" >つぶやく</button></p>
+      <p><input type="text" name="clubname" placeholder="clubname"></p>
+      <p><textarea type="text" name="lylics" placeholder="lylics"></textarea></p>
+      <p><textarea type="text" name="music" placeholder="music"></textarea></p>
+      <p><button type="submit" >投稿</button></p>
     </form>
-    <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+    <!-- clubname、lylics、music -->
+  <div>
+    <h3><?php echo $clubname; ?></h3>
+    <p><?php echo $lylics; ?></p>
+    <p><?php echo $music; ?></p>
+  </div>
 
 </body>
 </html>
